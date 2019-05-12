8b32f7511d3bbc03e3583bdf6ccbf9a568566a42
Marat Talantov <marat.talantov@gmail.com>
2019-05-11 22:26:54 -0400

    Saved

--- src/main/java/BoraTech/Batch2_UI_Automation/Constants.java
+++ src/main/java/BoraTech/Batch2_UI_Automation/Constants.java
@@ -1,5 +1,9 @@
 package BoraTech.Batch2_UI_Automation;
 
 public class Constants {
+	
+	public static final String DRIVER_PATH_MAC = "src/test/resource/chromedriver";
+	public static final String APPLICATION_URL = "file:///Users/marat/eclipse-workspace/Batch2_UI_Automation/src/test/resource/index.html";
+	
 
 }
