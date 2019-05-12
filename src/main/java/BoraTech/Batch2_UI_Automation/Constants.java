8f59f7be95f498f4fbdfdca4de9d134a04f4e79d
Waseem Zeid <zeidwaseem@gmail.com>
2019-05-11 20:33:16 -0400

    saving all changes

--- src/main/java/BoraTech/Batch2_UI_Automation/Constants.java
+++ src/main/java/BoraTech/Batch2_UI_Automation/Constants.java
@@ -1,5 +1,7 @@
 package BoraTech.Batch2_UI_Automation;
 
 public class Constants {
-
+	
+public static final String Driver_PATH_WINDOWS = "src/test/resource/chromedriver.exe";
+public static final String APPLICATION_URL = "file:///C:/Users/Waseem/eclipse-workspace/Batch2_UI_Automation/src/test/resource/index.html";
 }
