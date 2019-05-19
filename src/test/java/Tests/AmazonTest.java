070c12cbe49439c6e9058ba2a05a9b7aee0a5056
Muradil Erkin <Miller.muradil@gmail.com>
2019-05-19 14:53:14 -0400

    Changes to constants

--- src/test/java/Tests/AmazonTest.java
+++ src/test/java/Tests/AmazonTest.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Set;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByXPath;
 import org.openqa.selenium.ElementNotVisibleException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
@@ -16,76 +17,39 @@ import BoraTech.Batch2_UI_Automation.Keywords;
 public class AmazonTest {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", Constants.DRIVER_PATH_MAC);
-
-		WebDriver driver = new ChromeDriver();
-
-		driver.navigate().to("http://www.seleniumframework.com/Practiceform/");
-
-		String main = driver.getWindowHandle();
-		
-		driver.findElement(By.xpath("//button[text()='New Message Window']")).click();
-		
-		Keywords.waitFor(2);
-		
-		switchToNewWIndow(driver, main);
-		
-		System.out.println(driver.findElement(By.tagName("body")).getText());
-		
-		driver.close();
-		
-		driver.switchTo().window(main);
-		
-		driver.findElement(By.xpath("//button[text()='New Browser Tab']")).click();
-
-		switchToNewWIndow(driver, main);
-		
-		System.out.println(driver.getTitle());
-
-		driver.close();
-
-		driver.quit();
+		System.out.println(System.getProperty("user.dir"));
+		// https://docs.oracle.com/javase/tutorial/essential/environment/sysprop.html
 	}
 
 	public static void switchToNewWIndow(WebDriver driver, String main) {
 		String newWindowHandle = "";
 		for (String handle : driver.getWindowHandles()) {
-			if (!handle.equals(main)) newWindowHandle = handle;
+			if (!handle.equals(main))
+				newWindowHandle = handle;
 		}
 		driver.switchTo().window(newWindowHandle);
 	}
 
 	public static void searchAndAddToCart(WebDriver driver, String itemName, String itemLinktext) {
 		WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
-
 		searchBox.clear();
-
 		searchBox.sendKeys(itemName);
-
 		searchBox.submit();
-
 		List<WebElement> results = driver.findElements(By.cssSelector(".a-size-base-plus.a-color-base.a-text-normal"));
-
 		for (WebElement result : results) {
 			if (result.getText().equals(itemLinktext)) {
 				result.click();
 				break;
 			}
 		}
-
 		driver.findElement(By.id("add-to-cart-button")).click();
-
 		Keywords.waitFor(2);
-
 		try {
 			driver.findElement(By.id("attach-close_sideSheet-link")).click();
 			Keywords.waitFor(1);
 		} catch (NoSuchElementException e) {
-
 		} catch (ElementNotVisibleException e) {
-
 		}
-
 		driver.navigate().refresh();
 	}
 
