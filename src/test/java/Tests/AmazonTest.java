70d09502edf2a7d913bbbe36f8d1225c9699d234
Muradil Erkin <Miller.muradil@gmail.com>
2019-05-21 18:36:14 -0400

    File changes to index.html

--- src/test/java/Tests/AmazonTest.java
+++ src/test/java/Tests/AmazonTest.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 
 import BoraTech.Batch2_UI_Automation.Constants;
 import BoraTech.Batch2_UI_Automation.Keywords;
@@ -17,8 +19,38 @@ import BoraTech.Batch2_UI_Automation.Keywords;
 public class AmazonTest {
 
 	public static void main(String[] args) {
-		System.out.println(System.getProperty("user.dir"));
+//		System.out.println(System.getProperty("user.dir"));
 		// https://docs.oracle.com/javase/tutorial/essential/environment/sysprop.html
+
+		System.setProperty("webdriver.chrome.driver", Constants.CHROME_DRIVER_PATH_MAC);
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get(Constants.APPLICATION_URL);
+		
+		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[2]")));
+		
+		WebElement doubleClickButton = driver.findElement(By.id("doubleClick"));
+		
+		System.out.println(doubleClickButton.getAttribute("style"));
+		
+		Actions action = new Actions(driver);
+		
+		action.doubleClick(doubleClickButton).perform();
+		
+		Keywords.waitFor(2);
+		
+		System.out.println(doubleClickButton.getAttribute("style"));
+		driver.switchTo().defaultContent();
+		
+		driver.findElement(By.id("firstName")).sendKeys("Hello");
+		
+		Keywords.waitFor(2);
+		
+		driver.close();
+		
+		driver.quit();
+
 	}
 
 	public static void switchToNewWIndow(WebDriver driver, String main) {
