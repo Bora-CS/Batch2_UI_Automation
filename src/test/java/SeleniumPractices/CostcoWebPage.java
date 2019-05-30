b72231db899a81073b3a75687fc4cd07a87f78cc
Shirinay Kurban <shirinayk81@gmail.com>
2019-05-29 23:07:54 -0400

    Costco Page Automated

---
+++ src/test/java/SeleniumPractices/CostcoWebPage.java
@@ -0,0 +1,51 @@
+package SeleniumPractices;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import BoraTech.Batch2_UI_Automation.Constants;
+import BoraTech.Batch2_UI_Automation.Keywords;
+
+public class CostcoWebPage {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", Constants.CHROME_DRIVER_PATH_MAC);
+		WebDriver driver = new ChromeDriver();
+
+		driver.navigate().to("https://www.costco.com/");
+		WebDriverWait wait = new WebDriverWait(driver, 40);
+		wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.xpath("/html[1]/body[1]/div[6]/div[2]/div[1]/div[1]/a[6]/img[1]")))
+				.click();
+		Keywords.waitFor(1);
+
+		List<WebElement> resultSearch = driver.findElements(By.className("product-tile-set"));
+		System.out.println(resultSearch.size());
+		resultSearch.get(0).click();
+		driver.findElement(By.id("add-to-cart-btn")).click();
+		WebDriverWait waitFor = new WebDriverWait(driver, 30);
+		waitFor.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(text(),'View Cart')]")))
+				.click();
+
+		String itemText = driver.findElement(By.className("product-title")).getText();
+
+		WebElement checkOut = driver.findElement(By.id("shopCartCheckoutSubmitButton"));
+		if (itemText.equals("Apple MacBook Pro 13.3\" - Intel Core i5 - 8GB Memory - 256GB SSD - Silver")
+				&& checkOut.isDisplayed()) {
+			System.out.println("Test passed: item added to cart successfully.");
+
+		}
+
+		driver.close();
+		driver.quit();
+
+	}
+
+}
