7033fa96b70c14c0673dc5af4e7062f22f34142e
Shirinay Kurban <shirinayk81@gmail.com>
2019-06-01 10:25:41 -0400

    Changes

--- src/test/java/SeleniumPractices/WaitCommands.java
+++ src/test/java/SeleniumPractices/WaitCommands.java
@@ -1,10 +1,13 @@
 package SeleniumPractices;
 
+import java.time.Duration;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import BoraTech.Batch2_UI_Automation.Constants;
@@ -21,14 +24,21 @@ public class WaitCommands {
 //***************************Implicit Wait***************************************
 		System.setProperty("webdriver.chrome.driver", Constants.CHROME_DRIVER_PATH_MAC);
 		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);//don't wait too long
 		
 //***************************Explicit Wait***************************************
-		WebDriverWait waitFor = new WebDriverWait(driver, 15);
+		WebDriverWait waitFor = new WebDriverWait(driver, 15);//polling every 500 ms
 		waitFor.until(ExpectedConditions.alertIsPresent());//waited until alert show up
 		
+//***************************Fluent Wait***************************************
+//		A WebDriverWait is a fluent wait
+//		configure the timeout, polling time
+		FluentWait<WebDriver> advancedWait = new FluentWait<WebDriver>(driver)
+												.withTimeout(Duration.ofSeconds(30))
+												.pollingEvery(Duration.ofSeconds(2))
+												.ignoring(NoSuchElementException.class);
 		
-		
+		//advancedWait.until();
 		
 	}
 
