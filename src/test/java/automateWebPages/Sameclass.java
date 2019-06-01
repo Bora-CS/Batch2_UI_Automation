8040edf5f206b5cce785379c7e8388ada615f9c7
Nafisa Xiaokat <nanfeise@gmail.com>
2019-06-01 15:39:06 -0400

    changed

--- src/test/java/automateWebPages/Sameclass.java
+++ src/test/java/automateWebPages/Sameclass.java
@@ -3,7 +3,6 @@ package automateWebPages;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
-import org.openqa.selenium.ElementNotVisibleException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -22,63 +21,92 @@ public class Sameclass {
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://bora-test.s3-website-us-east-1.amazonaws.com");
 		driver.navigate().to("http://bora-test.s3-website-us-east-1.amazonaws.com");
-
-		String signup1 = driver.findElement(By.xpath("//a[@href=‘register.html’]")).getText();        
-		String signup2 = driver.findElement(By.cssSelector("btn.btn-lg.btn-info.mr-2")).getText();
-		System.out.println(signup1.equals(signup2));
-	
-		driver.findElement(By.xpath("//a[text()='Sign Up']")).click();
-		driver.findElement(By.id("name")).sendKeys("Nafisa");
+		String element = driver.findElement(By.cssSelector(".display-3.mb-4")).getText();
 		
-		Keywords.waitFor(2);
-		
-		List<WebElement> maleorfemale = driver.findElements(By.cssSelector(".form-check-input"));
-
-		maleorfemale.get(1).findElement(By.id("female")).click();
-
-		Keywords.waitFor(2);
-		
-		driver.findElement(By.id("email")).sendKeys("Nanfeise@gmail.com");
-		Keywords.waitFor(2);
-		
-		WebElement selectStateElement = driver.findElement(By.id("Course"));
-		Select state = new Select(selectStateElement);
-		state.selectByVisibleText("Java Spring Full Stack Development");
-		
-		Keywords.waitFor(2);
-
-		driver.findElement(By.id("password")).sendKeys("12345678");
-		Keywords.waitFor(2);
-		
-		driver.findElement(By.id("password2")).sendKeys("12345678");
-		Keywords.waitFor(2);
-		
-		driver.findElement(By.xpath("//*[@id=\"notarobot\"]")).click();
-		
-		Keywords.waitFor(2);
-		
-		driver.findElement(By.xpath("/html/body/div/div/div/div/form/input")).click();
-		Keywords.waitFor(2);
-		
-		
-		WebElement submit = driver.findElement(By.xpath("./html/body/div/div/div/div/form/input"));
-		
-		String text = submit.getText();
-		
-		if (submit.isEnabled() && text.contains("Congratulations!"))
-		{ 
-		    System.out.println("Successfully completed");
-		    
-		}else{
-			
-		    System.out.println("Not Successfully ");
+		if(element != null){
+			System.out.println("Bora UI Automation Test is checked");
+		} else {
+			System.out.println("Bora UI Automation Test is unchecked");
 		}
+		driver.findElement(By.xpath("//a[@href=‘students.html’]")).click(); 
+        List<WebElement> Studentelements = driver.findElements(By.cssSelector(".table table-hover"));  
+        String StudentsInformation= Studentelements.get(0).findElement(By.xpath("//p[@class='Students Information']")).getText();
+		System.out.println(StudentsInformation);
+		Studentelements.get(0).click();
 		
-	     driver.findElement(By.xpath("//a[@href=‘landing.html’]")).click(); 
+		driver.findElement(By.id("studentInfo")).click();
+       
+		String Students = driver.findElement(By.className("Students Information")).getText();
 		
+		if (!StudentsInformation.equalsIgnoreCase(Students)) {
+			System.out.println("Students Information all in here");
+		}
+		if (!driver.findElement(By.id("studentInfo")).isDisplayed()) {
+			System.out.println("Students Information is not displayed");
+		}
 
-
+		driver.close();
+		driver.quit();
 		
+		
+	}
+
+}
+
+	
+	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+				"src/Resources/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://bora-test.s3-website-us-east-1.amazonaws.com");
+		driver.navigate().to("http://bora-test.s3-website-us-east-1.amazonaws.com");
+		String login1 = driver.findElement(By.xpath("//a[@href=‘login.html’]")).getText();        
+		String login2 = driver.findElement(By.cssSelector("btn.btn-lg.btn-info.mr-2")).getText();
+		System.out.println(login1.equals(login2));
+		driver.findElement(By.xpath("//a[text()='Login']")).click();
+		 Keywords.waitFor(2);
+		driver.findElement(By.id("email")).sendKeys("Nanfeise@gmail.com");
+			Keywords.waitFor(2);
+		driver.findElement(By.id("password")).sendKeys("12345678");
+			Keywords.waitFor(2);
+			
+WebElement submit = driver.findElement(By.xpath("./html/body/div/div/div/div/form/input"));
+			
+			String text = submit.getText();
+			
+			if (submit.isEnabled() && text.contains("Student Profile"))
+			{ 
+			    System.out.println("Successfully completed");
+			    
+			}else{
+				
+			    System.out.println("Not Successfully ");
+			}
+			driver.findElement(By.xpath("///*[@id=\"mobile-nav\"]/ul[2]/li]")).click();
+			
+			Keywords.waitFor(2);
+			
+			driver.findElement(By.xpath("//*[@id=\"exampleModalCenter\"]/div/div/div[3]/button")).click();
+			
+			Keywords.waitFor(2);
+			
+			WebElement logout = driver.findElement(By.xpath("//*[@id=\"exampleModalCenter\"]/div/div/div[3]/button")); 
+					
+             String text2 = logout.getText();
+			
+			if (logout.isEnabled() && text2.contains("Sure, I will stay"))
+			{ 
+			    System.out.println("Successfully completed");
+			    
+			}else{
+				
+			    System.out.println("Not Successfully ");
+			}
+			String Gotta = driver.findElement(By.xpath("//a[@href=‘landing.html’]")).getText(); 
+			
+			
+			
 		driver.close();
 		driver.quit();
 
